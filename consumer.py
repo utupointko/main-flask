74461e3e7a10c2ee9c7ec533b270abe4ae46fa98
Arslan Abdizhalilov <arslan.abdizhalilov@gmail.com>
2021-04-21 16:01:17 +0200

    Add Flask Producer and Consumer

---
+++ consumer.py
@@ -0,0 +1,23 @@
+import pika
+
+params = pika.URLParameters('amqps://vaihkxgq:WNZrLPL6jg44KIhv0QEoEDk_ButDDPmb@cow.rmq2.cloudamqp.com/vaihkxgq')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='main')
+
+
+def callback(ch, method, properties, body):
+    print('Receiving in main')
+    print(body)
+
+
+channel.basic_consume(queue='main', on_message_callback=callback)
+
+print('Started Consuming')
+
+channel.start_consuming()
+
+channel.close()
